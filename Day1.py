75bb5d25155f37df1ff64fb7e2400449e5400f54
Keerthi-6301 <marakakeerthi73@gmail.com>
2026-07-28 13:34:34 +0530

    Tokens

---
+++ Day1.py
@@ -0,0 +1,54 @@
+#IDLE is color coding Environment
+#hashtag is used as single line comment...
+#we use triple quotes (single/double) ''',""" as Multiline comments
+#print("Hello codegnan")
+
+'''
+Tokens--> A Smallest unit in a program. Ther are 5 types of tokens..
+->Keywords
+->identifiers
+->literals
+->lirators
+->punctuators
+'''
+#keywords--> These are reserved words in python.we have 35 keywords...
+
+name = 'codegnan'
+print(name)
+email_id = 'marakakeerthi73@gmail.com'
+#print(Email_id)# python is case-Sensitive -->NameError
+#1swer = 23
+#print(1swer)
+#@derrrrr = 3.4 #no special characters
+
+#True = 45 # no keywords
+
+batch = "da23"
+print(batch)
+
+#variable -->named memory location where we store thr data,its a placeholder...
+batch = 'cse'
+print(batch)
+
+batch =23
+print(batch)
+
+#Litearls--> These are constants(value(s))--> number,character
+
+#Operators -->Opearators perform specific operation -->7type
+length = 5
+breadth = 3
+area = length * breadth
+print(area)
+
+
+#Multiassignment of variables
+name = 'codegnan'
+print(name)
+
+name,location,age = 'codegnan','Hyd',7
+print(name)
+print(location)
+print(age)
+
+
