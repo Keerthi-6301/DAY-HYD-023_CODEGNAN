8fe5ea1da10dc73c131d15b9c77f8b23b6459b07
Keerthi-6301 <marakakeerthi73@gmail.com>
2026-08-05 22:33:31 +0530

    stringspratice_day8

---
+++ stringspratice_day8.py
@@ -0,0 +1,27 @@
+def get_alphabet():
+    result = ""
+    for i in range(26):
+        result += chr(65 + i)+" "
+    return result
+print(get_alphabet())
+
+
+
+#Hippopotamus
+#observe +ve,+ve,-ve,-ve&+ve,-ve all possibilities
+animal='Hippopotamus'
+print(animal)
+print(len(animal))
+#positivity
+print(animal[:])
+print(animal[3:])
+print(animal[:4])
+#Negativity
+print(animal[-4:])
+print(animal[:-3])
+#positivity,Negativity
+print(animal[2:-4])
+print(animal[-1:2])
+print(animal[3::-2])
+
+
